7128c077eced6de8cddda9724a507166fe4a188e
Kale <13501991+AlbusNoir@users.noreply.github.com>
2021-01-28 17:19:57 -0500

    repo cleanup

--- permutation.py
+++
@@ -1,8 +0,0 @@
-# generates perms based on what's passed in
-
-import itertools
-# uncomment to use static assign
-# for p in itertools.permutations('ABCD'):
-perm = input('Enter something to permutate: ')
-for p in itertools.permutations(perm):
-    print(p)
